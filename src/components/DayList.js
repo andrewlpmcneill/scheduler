cd5e25ea2386ea7930ce1c07ab13e188a00971e5
Andrew McNeill <andrew@andrewmc.ca>
2022-02-23 15:12:49 -0400

    adds selected prop

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,7 @@ import DayListItem from "./DayListItem";
 export default function DayList (props) {
 
   const listItems = props.days.map(item => {
-    return <DayListItem {...item} setDay={props.setDay} />
+    return <DayListItem {...item} key={item.id} setDay={props.setDay} selected={item.name === props.day} />
   })
 
   return (
