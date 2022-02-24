922d2e6b75f470eb54c8e6c5ade3c3136174a225
Andrew McNeill <andrew@andrewmc.ca>
2022-02-24 00:07:02 -0400

    Adds InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "components/InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList (props) {
+
+  const interviewerItems = props.interviewers.map(item => {
+    return (
+      <InterviewerListItem
+        key={item.id}
+        name={item.name}
+        avatar={item.avatar}
+        selected={item.id === props.value}
+        setInterviewer={(event) => props.onChange(item.id)}
+      />
+    );
+  })
+
+  return (
+
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">
+        Interviewer
+      </h4>
+      <ul className="interviewers__list">
+        {interviewerItems}
+      </ul>
+    </section>
+
+  )
+
+}
