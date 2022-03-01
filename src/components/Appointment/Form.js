8ef029152dfa6eb7a3490f05e52802d7e4da4706
Andrew McNeill <andrew@andrewmc.ca>
2022-02-28 22:24:53 -0400

    adds form submission props and logic, onClick function call for saving form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -22,7 +22,9 @@ export default function Form(props) {
 
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={event => {
+          event.preventDefault();
+        }}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -41,7 +43,9 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={() => {
+            props.onSave(student, interviewer)
+          }}>Save</Button>
         </section>
       </section>
     </main>
