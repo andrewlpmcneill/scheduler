12c4c916af2055388941e62efaa90516f4fef852
Andrew McNeill <andrew@andrewmc.ca>
2022-02-28 22:21:56 -0400

    adds getInterview and getInterviewerForDay helper functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,3 +11,35 @@ export function getAppointmentsForDay(state, day) {
   return output;
 
 };
+
+export function getInterview(state, interview) {
+
+  if (!interview) return null; 
+
+  const interviewerIDString = String(interview.interviewer)
+  return {
+
+    "student": interview.student,
+    "interviewer": {
+      "id": interview.interviewer,
+      "name": state.interviewers[interviewerIDString].name,
+      "avatar": state.interviewers[interviewerIDString].avatar
+    }
+
+  };
+
+};
+
+export function getInterviewersForDay(state, day) {
+
+  const output = [];
+  for (const key of state.days) {
+    if (key.name === day) {
+      key.interviewers.forEach(interviewer => {
+        output.push(state.interviewers[interviewer]);
+      })
+    };
+  }
+  return output;
+
+}
