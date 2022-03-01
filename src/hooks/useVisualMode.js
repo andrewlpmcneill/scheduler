abd740be605c013e79f32c052dd36d4106d8926f
Andrew McNeill <andrew@andrewmc.ca>
2022-02-28 22:26:04 -0400

    creates useVisualMode custom hook for app rendering logic

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+  console.log(`History is`, history);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    const newHistory = history.slice(0, history.length - 1);
+    newHistory.push(newMode);
+    if (replace) {
+      setHistory(newHistory);
+    } else {
+      setHistory(prevHistory => [...prevHistory, newMode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length === 1) return;
+    history.pop();
+    setMode(history[history.length - 1]);
+  };
+
+
+  return { mode, transition, back };
+}
