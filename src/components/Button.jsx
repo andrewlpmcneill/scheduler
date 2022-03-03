3887f6c68a84f1f6916e00364ea37fa53037615c
Andrew McNeill <andrew@andrewmc.ca>
2022-03-02 22:59:43 -0400

    minor indentation refactor

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,9 +12,9 @@ export default function Button(props) {
 
    return (
    
-   <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
-      {props.children}
-   </button>
-      
+      <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
+         {props.children}
+      </button>
+   
    );
 }
